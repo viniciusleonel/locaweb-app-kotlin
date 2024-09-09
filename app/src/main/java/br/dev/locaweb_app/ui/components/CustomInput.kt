dd7609bb24e6f93274c3d0e0c9d76702c7e9c684
vinicius-leonel <viniciuslps.cms@gmail.com>
2024-09-09 15:05:42 -0300

    fix: alterando o componente CustomInput, deixando com cores customizaveis

--- app/src/main/java/br/dev/locaweb_app/ui/components/CustomInput.kt
+++ app/src/main/java/br/dev/locaweb_app/ui/components/CustomInput.kt
@@ -3,29 +3,37 @@ package br.dev.locaweb_app.ui.components
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CornerBasedShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Person
 import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.input.KeyboardCapitalization
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import br.dev.locaweb_app.ui.theme.ShapeButton
 
 @Composable
 fun CustomInput(
     modifier: Modifier = Modifier,
+    textStyle: TextStyle? = null,
+    color: Color? = null,
+    cornerShape: CornerBasedShape? = null,
     icon: ImageVector? = null,
     iconDescription: String? = "",
     label: String? = null,
@@ -38,7 +46,7 @@ fun CustomInput(
     var text by remember {
         mutableStateOf("")
     }
-    TextField(
+    OutlinedTextField(
         modifier = modifier
             .fillMaxWidth()
             .padding(20.dp, 10.dp),
@@ -65,7 +73,18 @@ fun CustomInput(
             keyboardType = keyboard,
             capitalization = capitalization
         ),
-        visualTransformation = if (isPassword) PasswordVisualTransformation() else VisualTransformation.None
+        visualTransformation = if (isPassword) PasswordVisualTransformation() else VisualTransformation.None,
+        textStyle = textStyle ?: TextStyle(Color.Black),
+        colors = TextFieldDefaults.colors(
+            focusedLabelColor = color ?: Color.Black,
+            focusedIndicatorColor = color ?: Color.Black,
+            focusedLeadingIconColor = color ?: Color.Black,
+            focusedPlaceholderColor = Color.Gray,
+            focusedContainerColor = Color.Transparent,
+            unfocusedContainerColor = Color.Transparent
+        ),
+        singleLine = true,
+        shape = cornerShape ?: ShapeButton.small
     )
 }
 
@@ -74,23 +93,27 @@ fun CustomInput(
 private fun CustomInputPrev() {
     Column {
         CustomInput(
+            textStyle = TextStyle(Color.Blue),
             label = "Insert your username:",
             icon = Icons.Filled.Person,
-            capitalization = KeyboardCapitalization.Words
+            capitalization = KeyboardCapitalization.Words,
         )
         CustomInput(
+            textStyle = TextStyle(Color.Blue),
             label = "Email",
             keyboard = KeyboardType.Email,
-            capitalization = KeyboardCapitalization.None
+            capitalization = KeyboardCapitalization.None,
         )
         CustomInput(
+            textStyle = TextStyle(Color.Blue),
             label = "Numbers",
-            keyboard = KeyboardType.Number
+            keyboard = KeyboardType.Number,
         )
         CustomInput(
+            textStyle = TextStyle(Color.Blue),
             label = "Insert your password:",
             keyboard = KeyboardType.Password,
-            capitalization = KeyboardCapitalization.None
+            capitalization = KeyboardCapitalization.None,
         )
     }
 }
--- app/src/main/java/br/dev/locaweb_app/ui/screens/LoginScreen.kt
+++ app/src/main/java/br/dev/locaweb_app/ui/screens/LoginScreen.kt
@@ -3,21 +3,19 @@ package br.dev.locaweb_app.ui.screens
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Lock
 import androidx.compose.material.icons.filled.Person
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.input.KeyboardCapitalization
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import br.dev.locaweb_app.ui.components.CustomButton
 import br.dev.locaweb_app.ui.components.CustomInput
-import br.dev.locaweb_app.ui.theme.Blue
+import br.dev.locaweb_app.ui.theme.OceanBlue
 import br.dev.locaweb_app.ui.theme.ShapeButton
 import com.google.accompanist.systemuicontroller.rememberSystemUiController
 
@@ -30,7 +28,7 @@ fun LoginScreen(
     val systemUiController = rememberSystemUiController()
 
     systemUiController.setStatusBarColor(
-        color = Blue
+        color = OceanBlue
     )
 
     Column (
@@ -43,24 +41,30 @@ fun LoginScreen(
             label = "Insert your username:",
             placeholder = "Insert your username:",
             icon = Icons.Filled.Person,
-            capitalization = KeyboardCapitalization.Words
+            color = OceanBlue,
+            cornerShape = ShapeButton.medium
         )
         CustomInput(
             label = "Insert your password:",
             placeholder = "Insert your password:",
             icon = Icons.Filled.Lock,
             keyboard = KeyboardType.Password,
-            isPassword = true
+            isPassword = true,
+            color = OceanBlue,
+            cornerShape = ShapeButton.medium
         )
         CustomButton(
             onClick = {
                 navController?.navigate("profile")
             },
+            colorsList = buttonColors,
             text = "Login",
             cornerShape = ShapeButton.medium
         )
     }
 }
+
+val buttonColors = listOf(OceanBlue, Color.Blue,  OceanBlue)
 
 @Preview(showBackground = true, showSystemUi = true)
 @Composable
