0183dd739b88700b9789158394b8a91895d830c0
vinicius-leonel <viniciuslps.cms@gmail.com>
2024-09-18 19:28:27 -0300

    fix: alterando formatacao da data formatDate

--- app/src/main/java/br/dev/locaweb_app/utils/FormatDate.kt
+++ app/src/main/java/br/dev/locaweb_app/utils/FormatDate.kt
@@ -5,8 +5,9 @@ import java.time.format.DateTimeFormatter
 import java.util.Locale
 
 fun formatDate(dateString: String): String {
-    val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSSSS", Locale.getDefault())
-    val dateTime = LocalDateTime.parse(dateString, formatter)
+    val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss", Locale.getDefault())
+    val dateTime = LocalDateTime.parse(dateString.substring(0, 19), formatter)
     val outputFormatter = DateTimeFormatter.ofPattern("dd 'de' MMMM 'de' yyyy, HH:mm", Locale("pt", "BR"))
     return dateTime.format(outputFormatter)
 }
+
